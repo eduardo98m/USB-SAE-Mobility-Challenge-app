b17e969880ad39787c59c0d2ec7a308418e72251
eduardo98m <32850455+eduardo98m@users.noreply.github.com>
2021-10-23 00:15:04 -0400

    Added some pargraphs

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import pandas as pd
 
 from get_dataframes import get_crash_data_df, get_gridwise_data_df, get_intersections_clust_data_df, get_bikes_clust_data_df
 
+import plotly.graph_objects as go
+
+
 @st.cache()
 def get_graph(df,
               group,
@@ -19,7 +22,8 @@ def get_graph(df,
               ticks=0, 
               plt_f=px.bar,
               horizontal=False,
-              **kwargs
+              update_layout_kwargs = {},
+              **kwargs,
               ):
     
     if only_bikes: df = df[df['BICYCLE'] == 1]
@@ -55,7 +59,7 @@ def get_graph(df,
         else:
             fig = plt_f(data_to_plot, x=group, y="count", #color='#00059E',
                 title= title,
-                labels = labels,**kwargs)
+                labels = labels,**kwargs)        
     
     else:
         df_groups = df.groupby(group)
@@ -91,7 +95,8 @@ def get_graph(df,
                 title= title,
                 labels = labels,**kwargs)
 
-
+    
+    fig.update_layout(**update_layout_kwargs)
 
     return fig
 
@@ -128,7 +133,28 @@ year_list = list(df['CRASH_YEAR'].unique())
 st.title('USB-SAE Mobility Team')
 
 st.image("https://media.istockphoto.com/photos/panoramic-view-of-pittsburgh-and-the-3-rivers-picture-id1093811582?k=20&m=1093811582&s=612x612&w=0&h=-KpOZ2OHlG7g2-A5fAGTCB0GtNCNVhmgZCqbr8hbzNE=")
-st.text("[Párrafo introductorio]")
+
+st.markdown("""
+<div style="text-align: justify">
+
+Welcome to the USB-SAE Mobility Team interactive both for the SAE-AI mini challenge.
+
+The objective of our entry was to develop a series of recomendations that would improve mobility accessibility, 
+solving crucial social issues in Allegheny County, Pennsylvania, based mainly on data analisys.
+
+With that objective in mind,  and trough the use of data mining tools and extensive research
+we found that a good solution to imporve the mobility of the county citizens is to incetivice 
+cycling among the county residents,
+through the building of cycling infrastructure.
+
+While looking trough the data, We also found other useful insights that might be worth mentioning.
+
+
+In this application we show the tough process that went into developing our solution and the 
+diferent insight that we found in the data.
+</div>
+
+""",unsafe_allow_html=True)
 
 
 
@@ -142,17 +168,23 @@ with st.expander("🚗🛵🥡 Initial data analisys and insights"):
     # Ageupados por nivel de luz 
     # Agrupados porroad condition
 
-    
-    # Accidents vs Speed Limit
-    graph = get_graph(
-        df, 
-        'SPEED_LIMIT', 
-        x_label = 'Speed Limit (milles per hour)', 
-        y_label = 'Accident count', 
-        title = 'Total accidents per speed limit',
-    )
-    st.plotly_chart(graph, use_container_width=True)
+    st.markdown("""
+    <div style="text-align: justify">
 
+    We begin by analyzing the data looking for temporal patterns that show an increase or 
+    decrease of accidents at certain times. Therefore, we grouped the data by year, month 
+    and day of the week. For the first case, a sharp decrease in accidents can be observed 
+    between the years 2019 and 2020, which can be easily attributed to the CoVID-19 
+    pandemic and the subsequent lockdown measures taken by the state government. 
+    These measures drastically reduced the mobility of citizens and subsequently the 
+    likelihood of accidents on the road. Fewer cars on the roads, fewer accidents are 
+    likely to occur.
+
+    </div>
+
+    """,unsafe_allow_html=True)
+    
+  
     # Accidents vs Year
     graph = get_graph(
         df, 
@@ -168,6 +200,18 @@ with st.expander("🚗🛵🥡 Initial data analisys and insights"):
     )
     st.plotly_chart(graph, use_container_width=True)
 
+    st.markdown("""
+    <div style="text-align: justify">
+
+    Regarding the months, the ones that accumulate more accidents are January and December, 
+    probably due to two main factors: road conditions at that time is rainy and snowy, 
+    negatively affecting mobility; while Christmas and vacation events promote the 
+    mobilization of many people, increasing the likelihood of vehicle crashes. 
+
+    </div>
+
+    """,unsafe_allow_html=True)
+
     # Accidents vs Month
     graph = get_graph(
         df, 
@@ -179,6 +223,18 @@ with st.expander("🚗🛵🥡 Initial data analisys and insights"):
     )
     st.plotly_chart(graph, use_container_width=True)
 
+    st.markdown("""
+    <div style="text-align: justify">
+
+   The weekdays with the highest proportion of accidents are Thursdays, 
+   Fridays and Saturdays, as more people tend to go out to parties and gatherings, 
+   this is in constrast to Sundays and Mondays where people tend to stay at home and only 
+   go out to work.
+
+    </div>
+
+    """,unsafe_allow_html=True)
+
     # Accidents vs Day of Week
     graph = get_graph(
         df, 
@@ -187,21 +243,24 @@ with st.expander("🚗🛵🥡 Initial data analisys and insights"):
         y_label ='Accidents', 
         title = 'Total accidents per day of week',
         only_bikes=False,
+        update_layout_kwargs={'xaxis':{'categoryorder':'array', 
+        'categoryarray':['Monday','Thursday','Wednesday','Tuesday',
+                        'Friday', 'Saturday', 'Sunday']}},
     )
     st.plotly_chart(graph, use_container_width=True)
 
+    st.markdown("""
+    <div style="text-align: justify">
 
+   Then, we decided to study how much weather conditions affect the proportion of disasters, 
+   and indeed if the road is wet the proportion is high, corroborating its increase at the
+   end of the year due to winter. The reason why the dry road has the highest number of 
+   accidents is because it is the common condition of the road, not because 
+   being dry promotes the occurrence of disasters.
 
-    # Accidents vs Light Condition
-    graph = get_graph(
-        df, 
-        'ILLUMINATION', 
-        x_label ='Illumination condition', 
-        y_label ='Accidents', 
-        title = 'Total accidents by road illumination',
-        only_bikes=False,
-    )
-    st.plotly_chart(graph, use_container_width=True)
+    </div>
+
+    """,unsafe_allow_html=True)
 
     # Accidents vs Road Condition
     graph = get_graph(
@@ -214,9 +273,57 @@ with st.expander("🚗🛵🥡 Initial data analisys and insights"):
     )
     st.plotly_chart(graph, use_container_width=True)
 
-    st.write("""
-    [Párrafo Explicando la data]
-    """)
+    st.markdown("""
+    <div style="text-align: justify">
+
+    Another important factor that we took into consideration are the illumination 
+    conditions, which is also related to the weather since in winter the nights last 
+    longer. It can be noted that a significant proportion of accidents occur in areas 
+    without streetlighs, thus installing streetlights in those areas could be a 
+    significant step to solving this problem.
+    </div>
+
+    """,unsafe_allow_html=True)
+
+    
+
+    # Accidents vs Light Condition
+    graph = get_graph(
+        df, 
+        'ILLUMINATION', 
+        x_label ='Illumination condition', 
+        y_label ='Accidents', 
+        title = 'Total accidents by road illumination',
+        only_bikes=False,
+    )
+    st.plotly_chart(graph, use_container_width=True)
+
+    st.markdown("""
+    <div style="text-align: justify">
+
+    Lastly, we noticed that the number of accidents that occurred on roads with 25 mph 
+    speed limits is very high. This is a relevant insight, as it can be explained by two 
+    factors: that most roads have a 25 mph speed limit, but also that such a limit may not 
+    be appropriate for some of the roads where it is imposed, i.e., reducing the speed 
+    limit to 20 mph by introducing some traffic calming measures may result in a reduction 
+    of accidents on those roads.
+    </div>
+
+    """,unsafe_allow_html=True)
+
+    # Accidents vs Speed Limit
+    graph = get_graph(
+        df, 
+        'SPEED_LIMIT', 
+        x_label = 'Speed Limit (milles per hour)', 
+        y_label = 'Accident count', 
+        title = 'Total accidents per speed limit',
+    )
+    st.plotly_chart(graph, use_container_width=True)
+
+
+
+
 
 
 with st.expander("🚗🛵🥡 Delivery data insights"):
@@ -237,9 +344,16 @@ with st.expander("🚦 The intersection problem"):
     #@Amin ponme esta gráfica bonita (Si se pudiesen colocar los porcentajes estaría peppa)
     plot_data = pd.DataFrame(df['INTERSECTION'].value_counts())
     st.plotly_chart(px.bar(plot_data), use_container_width=True)
-    st.write("""
-    [Párrafo Explicando la data]
-    """)
+    st.markdown("""
+    <div style="text-align: justify">
+    One of the most important features we found in the data is that around 40% of all 
+    accidents occurred near intersections. This means that intersections are a
+    focal point that increases the proportion of traffic accidents. 
+    The following map shows the distribution of crashes at different
+    intersections in the County.
+    </div>
+
+    """,unsafe_allow_html=True)
 
     st.subheader("Intersection Crashes clusterized")
 
@@ -255,10 +369,10 @@ with st.expander("🚦 The intersection problem"):
     The map below shows the number of bike accidents by year, there it is possible to 
     observe that a majority of the crashes occur near intersections.
     """)
-    bk_crahs_options = year_list + ["2004-2020"]
+    bk_crash_options = year_list + ["2004-2020"]
     bike_crash_year = st.selectbox("Year",
-                                    bk_crahs_options,
-                                    len(bk_crahs_options)-1
+                                    bk_crash_options,
+                                    len(bk_crash_options)-1
                                     )  
     
     if bike_crash_year != "2004-2020":
@@ -272,7 +386,7 @@ with st.expander("🚦 The intersection problem"):
                     lon="DEC_LONG", 
                     color="BICYCLE_DEATH_COUNT", 
                     size='BICYCLE_COUNT',
-                    size_max=15, zoom=10))
+                    size_max=15, zoom=10),use_container_width=True)
 
     #@Amin ponme esta gráfica bonita  (Si se pudiesen colocar los porcentajes estaría peppa)
     plot_data = pd.DataFrame(df['INTERSECTION'].loc[df['BICYCLE']==True].value_counts())
@@ -288,9 +402,10 @@ with st.expander("🚦 The intersection problem"):
                     opacity=0.55, 
                     #color_continuous_scale= px.colors.sequential.turbo,
                     size='CRASH_YEAR',# text = 'CLUSTER_COUNT',
-                    size_max=8, zoom=10))
+                    size_max=8, zoom=10),use_container_width=True)
     
-    st.write("""
+    st.markdown("""
+    ## Protected Intersections
     A proven meassure to reduce these kind of accidents are protected intersections,
     These are a special type of intersection where the cyclist and pedestrians are 
     separated from cars by a buffer zone, and drivers gain wider visibility and thus 
@@ -315,6 +430,21 @@ with st.expander("📈 Economics"):
 
 
 with st.expander("📊 Datasets"):
+
+    st.write("""
+    The data used for this study is the Allegheny County Crash Data, it is a dataset 
+    containing information about the different car crashes and accidents that occurred 
+    in the County between the years 2004 and 2020.  
+    The data set is well structured and contains lots of details abouteach accident. 
+    A data set provided by Gridwise was also employed to find some complementary insights 
+    for our proposal.
+    """)
+
+
+
+
+
+with st.expander("💡 Our proposal"):
 
     st.write("""
     The data used for this study is the Allegheny County Crash Data, it is a dataset 
